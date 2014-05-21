7a3d72c5bb2ab215204b36967bdffd1110852c23
Hǎiliàng Wáng <hwang.dev@gmail.com>
2014-05-21 19:15:27 +0800

    improve build script.

--- cover.sh
+++ cover.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Run test coverage on each subdirectories and merge the coverage profile.
+set -e
 
 echo "mode: count" > profile.cov
-fail=0
 
 # Standard go tooling behavior is to ignore dirs with leading underscors
 for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d);
 do
 if ls $dir/*.go &> /dev/null; then
-    go test -covermode=count -coverprofile=$dir/profile.tmp $dir || fail=1
+    go test -covermode=count -coverprofile=$dir/profile.tmp $dir
     if [ -f $dir/profile.tmp ]
     then
         cat $dir/profile.tmp | tail -n +2 >> profile.cov
