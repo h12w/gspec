657509599bfc88f0c6cac85e1d9bafe96d430bdc
Hǎiliàng Wáng <w@h12.me>
2015-05-13 18:42:15 +0800

    create a mgo session instead of the container.

--- db/mongo/mongo.go
+++ db/mongo/mongo.go
@@ -3,11 +3,34 @@ package mongo
 import (
 	"time"
 
+	"gopkg.in/mgo.v2"
 	"h12.me/gspec/db/docker"
 )
 
-func New(t docker.T) (*docker.Container, error) {
-	return docker.New(t, "mongo", 10*time.Second, func() (string, error) {
+type Session struct {
+	mgoSession
+	c *docker.Container
+}
+type mgoSession struct {
+	*mgo.Session
+}
+
+func (s *Session) Close() {
+	s.Session.Close()
+	s.c.Close()
+}
+
+func New(t docker.T) (*Session, error) {
+	container, err := docker.New(t, "mongo", 10*time.Second, func() (string, error) {
 		return docker.Run("-d", "-P", "mongo")
 	})
+	if err != nil {
+		return nil, err
+	}
+
+	session, err := mgo.Dial(container.Addr.String())
+	if err != nil {
+		return nil, err
+	}
+	return &Session{mgoSession{session}, container}, nil
 }
